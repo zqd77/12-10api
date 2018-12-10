f586ccf6200500a2a61a9ca3ee60212b794678a4
zq <759545434@qq.com>
2018-12-10 09:23:33 +0800

    完成接口开发

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var query = require('../mysql/configspl');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api/get/list', function(req, res, next) {
+    var page = req.query.page,
+        pagesize = req.query.pagesize,
+        type = req.query.type;
+    query('select * from aidou where type=?', [type], function(err, result) {
+        if (err) {
+            res.json({ code: 0, msg: err })
+        } else {
+            res.json({ code: 1, msg: result })
+        }
+    })
 });
 
 module.exports = router;
